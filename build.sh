93dc7707860e09e997f03d4dbf4cefb887137019
Knut Ahlers <knut@ahlers.me>
2018-11-19 11:39:44 +0100

    Work around Debian GnuPG bug
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=913614
    
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ export DEBIAN_FRONTEND=noninteractive
 # Install required utils
 apt-get update
 apt-get install -y --no-install-recommends \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  dirmngr \
-  git \
-  gnupg \
-  jq \
-  lsb-release \
-  make
+	apt-transport-https \
+	ca-certificates \
+	curl \
+	dirmngr \
+	git \
+	gnupg \
+	jq \
+	lsb-release \
+	make
 
 # Download latest release of inner-runner
 DOWNLOAD=$(curl -sSfL https://api.github.com/repos/repo-runner/repo-runner/releases/latest |
@@ -23,7 +23,7 @@ curl -sSfL "${DOWNLOAD}" | tar -xzf - -C /usr/local/bin
 mv /usr/local/bin/inner-runner* /usr/local/bin/inner-runner
 
 # Install docker-ce
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 9DC858229FC7DD38854AE2D88D81803C0EBFCD88
+apt-key adv --no-tty --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 9DC858229FC7DD38854AE2D88D81803C0EBFCD88
 
 echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -sc) stable" >/etc/apt/sources.list.d/docker.list
 apt-get update
@@ -31,9 +31,9 @@ apt-get install -y --no-install-recommends docker-ce
 
 # Cleanup
 apt-get purge -y \
-  dirmngr \
-  gnupg \
-  jq \
-  lsb-release
+	dirmngr \
+	gnupg \
+	jq \
+	lsb-release
 apt-get autoremove --purge -y
 rm -rf /var/lib/apt/lists/*
